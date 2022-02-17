66ed6372881ea093711ac6782387f9c38f9482a7
Silambarasan <rasisilambu9@gmail.com>
2022-02-17 21:29:02 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+interface Car {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +16,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'texteditor';
+  selectedValue: string='';
+  selectedCar: string='';
+  toggle = true;
+  status='UnBold';
+  bold_class='text_type_bold';
+  toggle_italics=true;
+  toggle_status='Italics';
+  toggle_underline=true;
+  underline_status='Underlined';
+  isbold=true;
+  isitalics=false;
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: '12'},
+    {value: 'pizza-1', viewValue: '14'},
+    {value: 'tacos-2', viewValue: '16'},
+    {value: 'tacos-2', viewValue: '20'},
+    {value: 'tacos-2', viewValue: '24'},
+    {value: 'tacos-2', viewValue: '32'},
+  ];
+
+  cars: Car[] = [
+    {value: 'volvo', viewValue: '12'},
+    {value: 'saab', viewValue: '24'},
+    {value: 'mercedes', viewValue: '16'},
+    {value: 'mercedes', viewValue: '20'},
+    {value: 'mercedes', viewValue: '24'},
+    {value: 'mercedes', viewValue: '32'},
+  ];
+
+bold() {
+    this.toggle = !(this.toggle);
+    this.status = this.toggle ? 'UnBold' : 'Bold';
+    this.isbold=false;
+    
+}
+italics(){
+  this.toggle_italics = !this.toggle_italics;
+  this.toggle_status = this.toggle_italics?'Italics':'Normal';
+  this.isitalics=false;
+}
+underlined(){
+  this.toggle_underline = !this.toggle_underline;
+  this.underline_status = this.toggle_underline?'Underlined':'Remove Underline';
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatButtonToggleModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
